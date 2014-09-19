668850d37947e99205dc83b6f20535f4237fb93c
Tyler Peterson <petersontb@familysearch.org>
2014-09-18 20:26:43 -0600

    parameterize repo

--- index.js
+++ index.js
@@ -5,42 +5,47 @@ var debug = require('debug')('cyan-git-stats');
 // Config properties
 var personalAccessToken = config.personalAccessToken;
 var team = config.teams[config.teams._order[0]];
-var repo = team.repos[0];
+var repos = team.repos;
 var agent = config.agent;
 
-debug('token', personalAccessToken);
-debug('repo', repo);
 
-request.get('https://api.github.com/repos/' + repo + '/stats/contributors')
-    .auth(personalAccessToken, 'x-oauth-basic')
-    .accept('application/vnd.github.v3+json')
-    .set('Agent', agent)
-    .end(function (res) {
-      if (res.ok) {
-        // debug('ok', JSON.stringify(res.body, null, '  '));
-        res.body.forEach(function (authorStatistics) {
-          var login = authorStatistics.author.login;
-          if (team.users.indexOf(login) === -1) {
-            return;
-          }
-          debug('author: ', login, authorStatistics.total);
-          var commits = 0;
-          var additions = 0
-          var net = 0;
-          var weeks = authorStatistics.weeks;
+function nextRepoStats() {
+  var repo = repos.shift();
+  debug('repo', repo);
 
-          if (weeks.length > 12) {
-            weeks = weeks.slice(-12);
-          }
+  request.get('https://api.github.com/repos/' + repo + '/stats/contributors')
+      .auth(personalAccessToken, 'x-oauth-basic')
+      .accept('application/vnd.github.v3+json')
+      .set('Agent', agent)
+      .end(function (res) {
+        if (res.ok) {
+          // debug('ok', JSON.stringify(res.body, null, '  '));
+          res.body.forEach(function (authorStatistics) {
+            var login = authorStatistics.author.login;
+            if (team.users.indexOf(login) === -1) {
+              return;
+            }
+            debug('author: ', login, authorStatistics.total);
+            var commits = 0;
+            var additions = 0
+            var net = 0;
+            var weeks = authorStatistics.weeks;
 
-          weeks.forEach(function (week) {
-            commits += week.c;
-            additions += week.a;
-            net += week.a - week.d;
-          });
-          debug('commits:', commits, 'additions:', additions, 'net:', net);
-        })
-      } else {
-        debug('not ok');
-      }
-    });
+            if (weeks.length > 12) {
+              weeks = weeks.slice(-12);
+            }
+
+            weeks.forEach(function (week) {
+              commits += week.c;
+              additions += week.a;
+              net += week.a - week.d;
+            });
+            debug('commits:', commits, 'additions:', additions, 'net:', net);
+          })
+        } else {
+          debug('not ok');
+        }
+      });
+}
+
+nextRepoStats();
