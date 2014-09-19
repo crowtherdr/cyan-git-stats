994d883c051031eace001a267e51e622c64888d3
Tyler Peterson <petersontb@familysearch.org>
2014-09-18 19:51:02 -0600

    Make a request against github's api.

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ var request = require('superagent');
 var config = require('./config');
 var debug = require('debug')('cyan-git-stats');
 
+// Config properties
 var personalAccessToken = config.personalAccessToken;
 
 debug('token', personalAccessToken);
+
+request.get('https://api.github.com/user')
+    .auth(personalAccessToken, 'x-oauth-basic')
+    .accept('application/vnd.github.v3+json')
+    .set('Agent', 'tylerpeterson')
+    .end(function (res) {
+      if (res.ok) {
+        debug('ok', JSON.stringify(res.body, null, '  '));
+      } else {
+        debug('not ok');
+      }
+    });
